8c8e96c682258e07fbf65fc7f5ae536568f22de9
김범석 <kqjatjr@gmail.com>
2023-01-23 18:18:57 +0900

    리코일 전역상태 추가

---
+++ src/store/recoilState.ts
@@ -0,0 +1,21 @@
+import { atom } from "recoil";
+
+type TRecord = {
+  activityControls: string[];
+  header: string;
+  products: string[];
+  subtitles: { name: string; url: string }[];
+  time: string;
+  title: string;
+  titleUrl: string;
+};
+type IStatistics = {
+  [key: number]: {
+    [key: number]: TRecord[];
+  };
+};
+
+export const dataState = atom({
+  key: "dataState",
+  default: {} as IStatistics,
+});
