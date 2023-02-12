50e059e961775c28bf90415cd0bdec5980be503e
bskim <bskim2@rsupport.com>
2023-02-12 17:08:10 +0900

    서비스 배경색 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,25 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Result from "./pages/result/Result";
+import styled from "styled-components";
 
 function App() {
   return (
     <RecoilRoot>
-      <div className="App">
+      <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/result" element={<Result />} />
         </Routes>
-      </div>
+      </Layout>
     </RecoilRoot>
   );
 }
 
 export default App;
+
+const Layout = styled.div`
+  background-color: #f1ece6;
+  min-width: 100vw;
+  min-height: 100vh;
+`;
