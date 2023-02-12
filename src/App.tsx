e968924190745957638aea2fc7205ba9a839939f
bskim <bskim2@rsupport.com>
2023-02-12 16:36:32 +0900

    테스트 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { RecoilRoot } from "recoil";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
-import Result from "./pages/result/result";
+import Result from "./pages/result/Result";
 
 function App() {
   return (
@@ -12,7 +12,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/result" element={<Result />} />
         </Routes>
-        <Result />
       </div>
     </RecoilRoot>
   );
